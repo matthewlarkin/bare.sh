a4f1b1ed3e76f11e623800ae3cf18682828e4c2f
Matthew Larkin <info@groveos.com>
2024-02-11 22:06:08 -0500

    Increases consistency in logical checks, adds quotes, removes unnecessary code

--- openai.sh
+++ openai.sh
@@ -5,8 +5,8 @@
 default_model="gpt-3.5-turbo-0125"
 
 function curlRequest() {
-    url=$1
-    payload=$2
+    url="$1"
+    payload="$2"
 
     if [[ -z "$payload" ]]; then
         response=$(curl -G "$url" --silent -H "Content-Type: application/json" -H "Authorization: Bearer $OPENAI_API_KEY" -H "OpenAI-Beta: assistants=v1" | jq -r)
@@ -33,7 +33,7 @@ case $1 in
         ;;
 
     "assistants.create")
-        [ -z "$2" ] || [ -z "$3" ] && { echo "🌿 ./openai.sh assistants.create [name] [instructions]" && exit 0; } || { name="$2"; instructions="$3"; }
+        [ -z "$2" ] && echo "🌿 ./openai.sh assistants.create [name] [instructions]" && exit 0 || { name="$2"; instructions="$3"; }
 
         [ -z "$4" ] && model=$default_model || model="$4"
 
@@ -47,7 +47,7 @@ case $1 in
         ;;
 
     "threads.create")
-        [ -z "$2" ] && { echo "🌿 ./openai.sh threads.create [initial_message]" && exit 0; } || initial_message="$2"
+        [ -z "$2" ] && echo "🌿 ./openai.sh threads.create [initial_message]" && exit 0 || initial_message="$2"
 
         payload=$(jq -n --arg initial_message "$initial_message" '{
             messages: [ { role: "user", content: $initial_message } ]
@@ -57,7 +57,7 @@ case $1 in
         ;;
 
     "thread.messages.append")
-        [ -z "$2" ] || [ -z "$3" ] && echo "🌿 ./openai.sh thread.messages.append [thread_id] [message]" && exit 0 || thread_id="$2" && message="$3"
+        [ -z "$2" ] && echo "🌿 ./openai.sh thread.messages.append [thread_id] [message]" && exit 0 || thread_id="$2" && message="$3"
 
         payload=$(jq -n --arg role "user" --arg content "$message" '{
             role: $role,
@@ -68,14 +68,14 @@ case $1 in
         ;;
 
     "thread.messages.list")
-        [ -z "$2" ] || [ -z "$3" ] && echo "🌿 ./openai.sh thread.messages.list [thread_id] (limit)" && exit 0 || thread_id="$2" && limit="$3"
+        [ -z "$2" ] && echo "🌿 ./openai.sh thread.messages.list [thread_id] (limit)" && exit 0 || thread_id="$2" && limit="$3"
         [[ ! $limit =~ ^[0-9]+$ ]] && echo "🚨 2:limit must be an integer" && exit 1
 
         curlRequest "https://api.openai.com/v1/threads/$thread_id/messages?limit=$limit" "" | jq  '{thread_id: .data[0].thread_id, messages: [.data[] | {role: .role, value: .content[0].text.value, created_at: .created_at}]}'
         ;;
 
     "thread.run")
-        [ -z "$2" ] || [ -z "$3" ] && echo "🌿 ./openai.sh thread.run [thread_id] [assistant_id]" && exit 0 || thread_id="$2" && assistant_id="$3"
+        [ -z "$2" ] && echo "🌿 ./openai.sh thread.run [thread_id] [assistant_id]" && exit 0 || thread_id="$2" && assistant_id="$3"
 
         payload=$(jq -n --arg assistant_id "$assistant_id" '{
             assistant_id: $assistant_id
@@ -85,7 +85,7 @@ case $1 in
         ;;
 
     "thread.run.poll")
-        [ -z "$2" ] || [ -z "$3" ] && { echo "🌿 ./openai.sh thread.run.poll [thread_id] [run_id]" && exit 0; } || thread_id="$2" && run_id="$3"
+        [ -z "$2" ] && echo "🌿 ./openai.sh thread.run.poll [thread_id] [run_id]" && exit 0 || thread_id="$2" && run_id="$3"
 
         curlRequest "https://api.openai.com/v1/threads/$thread_id/runs/$run_id" | jq -r '{thread_id: .thread_id, run_id: .id, status: .status}'
         ;;
