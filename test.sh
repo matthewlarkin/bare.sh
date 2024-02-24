dcabe859101cbbcca307afd5d9c81dba0144c8d2
Matthew Larkin <info@groveos.com>
2024-02-24 13:59:44 -0500

    removes test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# ubuntu 22.04
-# root@first:~/site# curl -sLO https://github.com/lovasoa/SQLpage/releases/download/v0.18.3/sqlpage-linux.tgz
-# root@first:~/site# tar -xzf sqlpage-linux.tgz
