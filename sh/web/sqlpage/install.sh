73b23c3b9b67f0819a38ff2f39f50072ccb9823c
Matthew Larkin <info@groveos.com>
2024-02-24 10:37:07 -0500

    updates sqlpage ownership

--- sh/web/sqlpage/install.sh
+++ sh/web/sqlpage/install.sh
@@ -5,10 +5,10 @@ bash web/nginx/install.sh
 sqlpage_bin="https://github.com/lovasoa/SQLpage/releases/download/v0.18.3/sqlpage-linux.tgz"
 
 # check if SQLPage is installed (and install it if it's not)
-if [ ! -f "/usr/bin/sqlpage" ]; then
+if [ ! -f "/bin/sqlpage" ]; then
     sudo curl -s -L -O $sqlpage_bin
     sudo tar -xzf sqlpage-linux.tgz && sudo rm sqlpage-linux.tgz
-    sudo mv sqlpage.bin /usr/bin/sqlpage
-    sudo chmod 750 /usr/bin/sqlpage
-    sudo chown www-data:www-data /usr/bin/sqlpage
+    sudo mv sqlpage.bin /bin/sqlpage
+    sudo chmod 750 /bin/sqlpage
+    sudo chown www-data:www-data /bin/sqlpage
 fi
